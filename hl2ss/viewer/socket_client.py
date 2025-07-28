499b403106dd19dd9d25fdac5df8944518d17d39
eleanor <eleanor.chen@winsor.edu>
2025-07-28 15:32:52 -0400

    idk

--- hl2ss/viewer/socket_client.py
+++ hl2ss/viewer/socket_client.py
@@ -12,4 +12,3 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     data = s.recv(1024)
 
 print(f"Received {data!r}")
-
--- hl2ss/viewer/socket_server.py
+++ hl2ss/viewer/socket_server.py
@@ -11,7 +11,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.listen()
     conn, addr = s.accept()
     with conn:
-        print(f"Connected by {addr}")
+        print(f"Connected to {addr}")
         while True:
             data = conn.recv(1024)
             if not data:
