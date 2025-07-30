b69f7dcb936cdf0a5a860f305edb7c120a729007
eleanorchen0 <eleanor.chen@winsor.edu>
2025-07-30 13:25:13 -0400

    Update server_unity.cs

--- hl2ss/viewer/server_unity.cs
+++ hl2ss/viewer/server_unity.cs
@@ -1,91 +1,39 @@
 using System;
-using System.Text;
 using System.Net;
 using System.Net.Sockets;
-using System.Threading;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEditor.VersionControl;
+using System.Text;
 
-public class server : MonoBehaviour
+class EchoServer
 {
-    TcpListener listener = null;
-    TcpClient client = null;
-    NetworkStream stream = null;
-    Thread thread;
-
-    // Start is called before the first frame update
-    private void Start()
+    static void Main()
     {
-        thread = new Thread(new ThreadStart(SetupServer));
-        thread.Start();
-    }
+        string host = "10.29.224.211";
+        int port = 65432;
 
-    // Update is called once per frame
-    private void Update()
-    {
-        if (Input.GetKeyUp(KeyCode.Space))
+        IPAddress ipAddress = IPAddress.Parse(host);
+        IPEndPoint localEndPoint = new IPEndPoint(ipAddress, port);
+
+        TcpListener server = new TcpListener(localEndPoint);
+
+        server.Start();
+        Console.WriteLine($"Listening on {host}:{port}");
+
+        TcpClient client = server.AcceptTcpClient();
+        Console.WriteLine("Connected to " + client.Client.RemoteEndPoint);
+
+        NetworkStream stream = client.GetStream();
+        byte[] buffer = new byte[1024];
+
+        while (true)
         {
-            SendMessageToClient("Hello");
+            int bytesRead = stream.Read(buffer, 0, buffer.Length);
+            if (bytesRead == 0)
+                break;
+
+            stream.Write(buffer, 0, bytesRead);
         }
-    }
 
-    private void SetupServer()
-    {
-        try
-        {
-            IPAddress localAddr = IPAddress.Parse("10.29.211.183");
-            listener = new TcpListener(localAddr, 65432);
-            listener.Start();
-
-            byte[] buffer = new byte[1024];
-            string data = null;
-
-            while (true)
-            {
-                Debug.Log("Waiting for connection");
-                client = listener.AcceptTcpClient();
-                Debug.Log("Connected");
-
-                data = null;
-                stream = client.GetStream();
-
-                int i;
-
-                while ((i = stream.Read(buffer, 0, buffer.Length)) != 0)
-                {
-                    data = Encoding.UTF8.GetString(buffer, 0, i);
-                    Debug.Log("Received: " + data);
-
-                    string response = "Server response: " + data.ToString();
-                    SendMessageToClient(message: response);
-                }
-                client.Close();
-            }
-        }
-        catch (SocketException e)
-        {
-            Debug.Log("SocketException: " + e);
-        }
-        finally
-        {
-            listener.Stop();
-        }
-    }
-
-    private void OnApplicationQuit()
-    {
-        stream.Close();
         client.Close();
-        listener.Stop();
-        thread.Abort();
-    }
-
-    private void SendMessageToClient(string message)
-    {
-        byte[] msg = Encoding.UTF8.GetBytes(message);
-        stream.Write(msg, 0, msg.Length);
-        Debug.Log("Sent: " + message);
+        server.Stop();
     }
 }
