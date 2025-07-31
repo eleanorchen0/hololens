aeda6e7794b67e084bd46240f15d51e7e4ae5d0c
eleanorchen0 <eleanor.chen@winsor.edu>
2025-07-31 10:54:33 -0400

    Update test.py

--- hl2ss/viewer/test.py
+++ hl2ss/viewer/test.py
@@ -19,24 +19,24 @@ profile = hl2ss.VideoProfile.H265_MAIN
 bitrate = None
 unity_host, unity_port = "127.0.0.1", 1984
 
-position = []
-rotation = []
-
-def average_time(time_position, current_time, time_interval):
-    time_position[:] = [(time, position) for (time, position) in time_position if (current_time - time <= time_interval)]
-
-    if not time_position:
-        return None
-
-    positions = np.array([position for (_, position) in time_position])
-    average_position = positions.mean(axis=0)
-    return average_position
-marker_length  = 0.07
-
-aruco_dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_50)
-aruco_parameters = cv2.aruco.DetectorParameters()
-aruco_half = marker_length/2
-aruco_reference = np.array([[-aruco_half, aruco_half, 0], [aruco_half, aruco_half, 0], [aruco_half, -aruco_half, 0], [-aruco_half, -aruco_half, 0], [0, 0, marker_length / 2]], dtype=np.float32)
+# position = []
+# rotation = []
+# 
+# def average_time(time_position, current_time, time_interval):
+#     time_position[:] = [(time, position) for (time, position) in time_position if (current_time - time <= time_interval)]
+# 
+#     if not time_position:
+#         return None
+# 
+#     positions = np.array([position for (_, position) in time_position])
+#     average_position = positions.mean(axis=0)
+#     return average_position
+# marker_length  = 0.07
+# 
+# aruco_dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_50)
+# aruco_parameters = cv2.aruco.DetectorParameters()
+# aruco_half = marker_length/2
+# aruco_reference = np.array([[-aruco_half, aruco_half, 0], [aruco_half, aruco_half, 0], [aruco_half, -aruco_half, 0], [-aruco_half, -aruco_half, 0], [0, 0, marker_length / 2]], dtype=np.float32)
 
 
 client = hl2ss_lnm.rx_rm_vlc(host, port, mode=mode, profile=profile, bitrate=bitrate)
