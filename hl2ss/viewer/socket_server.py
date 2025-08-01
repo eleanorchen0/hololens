668468f2408cab0f96275388337858d464772a87
eleanorchen0 <eleanor.chen@winsor.edu>
2025-08-01 10:26:21 -0400

    Update socket_server.py

--- hl2ss/viewer/socket_server.py
+++ hl2ss/viewer/socket_server.py
@@ -1,20 +1,19 @@
 import socket
-import sys
-import selectors
-import types
 
-HOST = "10.29.224.211"
-PORT = 65432
+host, port = "127.0.0.1", 1984
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print(f"Connected to {addr}")
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            conn.sendall(data)
+data = "1,1,1,1,1,1,1"
 
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind((host, port))
+server_socket.listen(1)
+
+print(f"Python Server listening on {host}:{port}")
+conn, addr = server_socket.accept()
+print(f"Connection from {addr}")
+
+try:
+    conn.sendall(data.encode('utf-8'))  # Echo back
+finally:
+    conn.close()
+    server_socket.close()
