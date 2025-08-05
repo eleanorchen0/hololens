d1998c84d6a7f3c3a38039dd7efc549a01cea3bf
eleanorchen0 <eleanor.chen@winsor.edu>
2025-08-05 14:20:43 -0400

    Update data_server.py

--- hl2ss/viewer/data_server.py
+++ hl2ss/viewer/data_server.py
@@ -159,13 +159,13 @@ bitrate = None
 #------------------------------------------------------------------------------
 unity_host, unity_port = "0.0.0.0", 1984
 
-# server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# server_socket.bind((unity_host, unity_port))
-# server_socket.listen(1)
-# 
-# print(f"Python Server listening on {unity_host}:{unity_port}")
-# conn, addr = server_socket.accept()
-# print(f"Connection from {addr}")
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind((unity_host, unity_port))
+server_socket.listen(1)
+
+print(f"Python Server listening on {unity_host}:{unity_port}")
+conn, addr = server_socket.accept()
+print(f"Connection from {addr}")
 
 #------------------------------------------------------------------------------
 position = []
@@ -255,7 +255,7 @@ while True:
 
         print(d)
 
-        # conn.sendall(d.encode('utf-8'))
+        conn.sendall(d.encode('utf-8'))
 
     cv2.imshow("wave aruco", cv2.rotate(color_frames, cv2.ROTATE_90_COUNTERCLOCKWISE))
 
